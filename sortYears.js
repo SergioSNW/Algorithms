5a4e4c334aa109139f483d44898ccd9aec9da86a
Sergio Santamaria Nora <83676008+SergioSNW@users.noreply.github.com>
2025-06-15 13:52:34 +0100

    Update sortYears.js

--- sortYears.js
+++ sortYears.js
@@ -1,6 +1,9 @@
 // Sorting years in descending order.
 // If you need to sort them in ascending order,
-// Line 5 has to be changed to <<< return arr.sort((a, b) => a - b);
+// Line 5 has to be changed to <<< return arr.sort((a, b) => a - b); >>>>>
+// For reference, the most basic use of Sort(), if you remember:
+// (a, b) => a - b /// this will sort numbers in ascending order,
+// if we use (a, b) => b - a /// , we will get them sorted in descending order.
 function sortYears(arr) {
   return arr.sort((a, b) => b - a);
 }
