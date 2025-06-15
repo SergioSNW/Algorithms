fe253701c7935ec5f05974dffbf7e24109f55e4f
Sergio Santamaria Nora <83676008+SergioSNW@users.noreply.github.com>
2025-06-15 10:50:37 +0100

    Create filterItems.js

---
+++ filterItems.js
@@ -0,0 +1,15 @@
+// Search mutual items in two sting arrays.
+function justCoolStuff(arr1, arr2) {
+  return arr1.filter(item => {
+    return arr2.includes(item);
+  })
+}
+
+
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+
+const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+
+console.log(justCoolStuff(myStuff, coolStuff))
+// Should print [ 'fruit-by-the-foot', 'skateboards', 'my room' ]
+
